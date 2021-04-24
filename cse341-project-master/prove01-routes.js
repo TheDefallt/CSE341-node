7e6f5e61992637007b44a284f166d7aa0929b441
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-04-23 20:13:15 -0700

    Update prove01-routes.js

--- cse341-project-master/prove01-routes.js
+++ cse341-project-master/prove01-routes.js
@@ -8,7 +8,11 @@ const requestHandler = (req, res) => {
         //Add a greeting message
         res.write('<html>');
         res.write('<head><title>Prove 01</title></head>');
-        res.write('<body><form action="/create-user" method="POST"><input type="username" name="username"><button type="submit">Send</button></form></body>');
+        res.write('<body>');
+        res.write('<h1>User Account creation</h1>');
+        res.write('<p>Welcome to the user account creation page</p>');
+        res.write('<form action="/create-user" method="POST"><input type="username" name="username"><button type="submit">Send</button></form>');
+        res.write('</body>');
         res.write('</html>');
         return res.end();
     }
@@ -18,7 +22,19 @@ const requestHandler = (req, res) => {
     }
 
     if (url === '/users' && method === 'POST') {
-        //Return a list of dummy users
+        res.write('<html>');
+        res.write('<head><title>Prove 01</title></head>');
+        res.write('<body>');
+        res.write('<h1>User Account list</h1>');
+        res.write('<ul>');
+        res.write('<li>TStark</li>');
+        res.write('<li>BBanner</li>');
+        res.write('<li>PParker</li>');
+        res.write('<li>NFury</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
         //Update list of users on the user's page.
     }
 }
