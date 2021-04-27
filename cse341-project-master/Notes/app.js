0e7a2ec979ac8782815444bb3762922c39621aac
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-04-26 22:22:16 -0700

    Week 2 v2

--- cse341-project-master/Notes/app.js
+++ cse341-project-master/Notes/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes.js');
@@ -13,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(__dirname(path.join__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
