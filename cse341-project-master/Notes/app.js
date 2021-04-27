71dc10416bb04fb3a96313876048cf397476349f
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-04-27 10:23:39 -0700

    Week 2 v3

--- cse341-project-master/Notes/app.js
+++ cse341-project-master/Notes/app.js
@@ -15,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname(path.join__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
