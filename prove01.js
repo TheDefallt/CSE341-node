ef0660b88274b6fc578e6877bbe778b5cacc8ff5
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-04-27 21:38:35 -0700

    small changes

--- prove01.js
+++ prove01.js
@@ -1,9 +1,7 @@
 const http = require('http');
 
-const port = process.env.port || 3000;
-
 const routes = require('./prove01-routes.js');
 
 const server = http.createServer(routes);
 
-server.listen(port);
+server.listen(process.env.PORT || 3000);
