f039140a4df6b12a1207409198b4afbd9bce701e
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-04-28 18:13:18 -0700

    Team changes

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,31 @@
 const express = require('express');
 const router = express.Router();
 
+const users = ["Tony Stark", "Bruce Banner", "Peter Parker"];
+
+const userExists = true;
+
 router.get('/',(req, res, next) => {
-    res.render('pages', { 
+    res.render('pages/ta02', { 
         title: 'Team Activity 02', 
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        users: users
     });
+});
+
+router.post('/addUser', (req, res, next) => {
+    users.push(req.body.username);
+    res.redirect('/ta02');
+});
+
+router.post('/removeuser', (req, res, next) => {
+    if (users.indexOf(req.body.userremove) < 0) {
+
+    }
+    users.splice(users.indexOf(req.body.userremove), 1);
+    res.redirect('/ta02');
 });
 
 module.exports = router;
