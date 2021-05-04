77602095368c11109ade8489b6308ac4061510a7
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-05-03 21:10:06 -0700

    Week3v4

---
+++ Notes/Controllers/shop.js
@@ -0,0 +1,37 @@
+const Product = require('../Models/product');
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('Shop/product-list', {
+            pageTitle: 'All products', 
+            prods: products, 
+            docTitle: 'Shop', 
+            path: '/products'
+        });
+    });
+};
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('Shop/index', {
+            pageTitle: 'Shop', 
+            prods: products, 
+            docTitle: 'Shop', 
+            path: '/'
+        });
+    });
+}
+
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart'
+    });
+}
+
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout'
+    });
+}
