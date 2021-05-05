50c645e541f46d3ed3c1e2c7dd140769f9ae4eae
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-05-04 21:43:46 -0700

    Week 3 v3

--- Notes/Controllers/shop.js
+++ Notes/Controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../Models/product');
+const Cart = require('../Models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
@@ -14,9 +15,13 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, netxt) => {
     const prodId = req.params.productId;
     Product.findById(prodId, product => {
-        
+        res.render('shop/product-detail', {
+            product: product, 
+            pageTitle: product.title,
+            path: '/products'
+        });
     });
-    res.redirect('/');
+    
 }
 
 exports.getIndex = (req, res, next) => {
@@ -35,6 +40,14 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart'
     });
+}
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    });
+    res.render('/cart');
 }
 
 exports.getOrders = (req, res, next) => {
