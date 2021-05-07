34a2c77e518589785e95e65d0c6fd6dcba5a82a4
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-05-06 22:24:48 -0700

    week 3 v2

--- zApp/app.js
+++ zApp/app.js
@@ -9,13 +9,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'zApp/views');
 
-//const adminRoutes = require('./Routes/admin.js');
+const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
