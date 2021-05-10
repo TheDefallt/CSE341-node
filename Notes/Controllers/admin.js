fd9437dc0fcfcb0c7578e55a5bb5a52097ad2df7
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-05-09 21:00:50 -0700

    Week4 v3

--- Notes/Controllers/admin.js
+++ Notes/Controllers/admin.js
@@ -16,8 +16,14 @@ exports.postAddProduct = (req, res, next) => {
     const description = req.body.description;
 
     const product = new Product(title, imageUrl, description, price);
-    product.save();
-    res.redirect('/');
+    product.save()
+    .then(result => {
+        console.log('Created Product');
+        res.redirect('/admin/products');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
