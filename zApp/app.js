44101712bde55f708358e90d8c75ed69ada74777
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-05-18 12:45:35 -0700

    Update app.js

--- zApp/app.js
+++ zApp/app.js
@@ -2,52 +2,76 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
+const session = require('express-session'); //Install express-session
+const MongoDBStore = require('connect-mongodb-session')(session); //Install connect-mongodb-session
+const csrf = require('csurf'); //Install csrf
+const flash = require('connect-flash'); //Install connect-flash
+
+const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 
+    'mongodb+srv://thedefallt:Shotgunshell@byui-classes.ydypa.mongodb.net/airplaneShop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'session'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'zApp/views');
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+app.use(csrfProtection);
+app.use(flash());
+
 app.use((req, res, next) => {
-    User.findById('609dfc95e65ce53c5000ea16')
+if (!req.session.user) {
+    return next();
+}
+User.findById(req.session.user._id)
     .then(user => {
-        req.user = user;
-        next();
+    req.user = user;
+    next();
     })
     .catch(err => console.log(err));
 });
 
+// app.use((req, res, next) => {
+//     User.findById('609dfc95e65ce53c5000ea16')
+//     .then(user => {
+//         req.user = user;
+//         next();
+//     })
+//     .catch(err => console.log(err));
+// });
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(
-    'mongodb+srv://thedefallt:Shotgunshell@byui-classes.ydypa.mongodb.net/airplaneShop?retryWrites=true&w=majority'
-)
+mongoose
+.connect(MONGODB_URI)
 .then(result => {
-    User.findOne()
-    .then(user => {
-        if (!user) {
-            const user = new User({
-                name: 'Admin',
-                email: 'admin@planes.com',
-                cart: {
-                    items: []
-                }
-            });
-            user.save();
-        }
-    });
     app.listen(process.env.PORT || 3000);
 })
 .catch(err => {
