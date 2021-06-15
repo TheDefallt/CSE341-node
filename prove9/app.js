4da6532f544105248a5c1841b5e14d77ab04a5c6
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-15 14:58:39 -0700

    Update app.js

--- prove9/app.js
+++ prove9/app.js
@@ -5,8 +5,7 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
