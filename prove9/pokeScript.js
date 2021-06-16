3d7e6ecb9863d3d92c01eb98fea0f654f013433e
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-16 13:31:22 -0700

    Prove9

--- prove9/pokeScript.js
+++ prove9/pokeScript.js
@@ -1,53 +1,79 @@
-const list = document.getElementById("swData")
-const prevB = document.getElementById("prev")
-const nextB = document.getElementById("next")
-let pageURLnext = "";
-let pageURLprev = "";
-let pagelist = 0;
-let navi = document.getElementById("navigate");
+//First and las URL's
+const firstURL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10";
+const lastURL = "https://pokeapi.co/api/v2/pokemon?offset=1110&limit=10";
 
-fetch("https://swapi.dev/api/starships/")
+//Placeholders for dynamic URL's
+let currentURL = '';
+let nextURL = '';
+let prevURL = '';
+
+//Page elements
+const list = document.getElementById("pokeList");
+const firstB = document.getElementById("first");
+const prevB = document.getElementById("previous");
+const currentB = document.getElementById("current");
+const nextB = document.getElementById("next");
+const lastB = document.getElementById("last");
+
+//Fetch a page by URL
+const getPageByURL = url => {
+    fetch(url)
     .then(response => response.json())
-    .then((data) => {
-        pagelist = (Math.ceil(data.count / 10))
-        for (let i = 1; i <= pagelist; i++) {
-            let num = document.createElement("input")
-            num.setAttribute("type", "button");
-            num.setAttribute("value", i);
-            navi.appendChild(num)
-            num.addEventListener('click', () => fetchData(`https://swapi.dev/api/starships/?page=${i}`), false)
-        }
+    .then((responseData) => {
+        //Clears the list for the next fetch
+        list.innerHTML = '';
+
+        //Loops through elements in the response and adds line items to the ul list for each of them
+        responseData.results.forEach(element => {
+            list.innerHTML += `<li>${element.name}</li>`
+        });
+
+        //Sets the url variables to the proper variables for the buttons.
+        currentURL = url;
+        nextURL = responseData.next;
+        prevURL = responseData.previous;
     });
+}
 
-fetchData("https://swapi.dev/api/starships/")
+//Trigger getPageByURL for the first page
+const getFirstPage = () => {
+    getPageByURL(firstURL);
+}
 
-function fetchData(fetchURL) {
-    list.innerHTML = "";
-    if (fetchURL !== null) {
-        fetch(fetchURL)
-        .then(response => response.json())
-        .then((data) => {
-            let obj = Object.entries(data.results);
-            pageURLnext = data.next;
-            pageURLprev = data.previous;
-            if(pageURLnext !== null && pageURLnext.search('https:') <0) {
-                pageURLnext = pageURLnext.replace('http:', 'https:')
-            }
-            if(pageURLprev !== null && pageURLprev.search('https:') <0) {
-                pageURLprev = pageURLprev.replace('http:', 'https:')
-            }
-            obj.forEach(item => {
-                let lItem = document.createElement("li");
-                lItem.textContent = item[1].name;
-                list.appendChild(lItem);
-            })
-        })
-    } else {
-        let lItem = document.createElement("li");
-        lItem.textContent = "No items found";
-        list.appendChild(lItem);
+//Trigger getPageByURL for the previous page
+const getPrevPage = () => {
+    if (prevURL != null){
+        getPageByURL(prevURL);
+    }else{
+        return;
     }
 }
-prevB.addEventListener('click', () => fetchData(pageURLprev), false);
-nextB.addEventListener('click', () => fetchData(pageURLnext), false);
 
+//Trigger getPageByURL for the current page
+const getCurrentPage = () => {
+    getPageByURL(currentURL);
+}
+
+//Trigger getPageByURL for the next page
+const getNextPage = () => {
+    if (nextURL != null){
+        getPageByURL(nextURL);
+    }else{
+        return;
+    }
+}
+
+//Trigger getPageByURL for the last page
+const getLastPage = () => {
+    getPageByURL(lastURL);
+}
+
+//Initialize page with first URL
+getPageByURL(firstURL);
+
+//Add event listeners to each button for their various functions.
+firstB.addEventListener('click', getFirstPage);
+prevB.addEventListener('click', getPrevPage);
+currentB.addEventListener('click', getCurrentPage);
+nextB.addEventListener('click', getNextPage);
+lastB.addEventListener('click', getLastPage);
