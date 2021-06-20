949c4647c08cdc334ae2cc97ed0a009c5002fd2e
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-19 22:07:32 -0700

    Update pokeScript.js

--- prove9/public/pokeScript.js
+++ prove9/public/pokeScript.js
@@ -15,11 +15,8 @@ const currentB = document.getElementById("current");
 const nextB = document.getElementById("next");
 const lastB = document.getElementById("last");
 
-console.log("pokeScript");
-
 //Fetch a page by URL (Put this in a model?)
 const getPageByURL = url => {
-    console.log("Making it here");
     fetch(url)
     .then(response => response.json())
     .then((responseData) => {
