e18ab6d58fb9a37a469c474fa84be590ce9ef230
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-23 17:20:45 -0700

    Prove 10 v3

--- prove10/app.js
+++ prove10/app.js
@@ -1,7 +1,11 @@
-const express = require('express');
 const path = require('path');
-const router = express.Router();
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
 
 // Path to your JSON file, although it can be hardcoded in this file.
 const dummyData = require('./data.json');
@@ -10,24 +14,19 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', 'prove10');
 
-router.get('/fetchAll', (req, res, next) => {
-    console.log("Fetching List");
+app.use('/fetchAll', (req, res, next) => {
     res.json(dummyData);
 });
 
-router.post('/insert', (req, res, next) => {
-    /************************************************
-     * INSERT YOUR WEB ENDPOINT CODE HERE
-     ************************************************/
+app.use('/insert', (req, res, next) => {
+    console.log(req.body.newHero);
+    dummyData.avengers.push({name: req.body.newHero});
 });
 
 app.get('/', (req, res, next) => {
     res.render('ta10', {
-        title: 'Team Activity 10',
-        path: '/ta10'
+        title: 'Hero Activity'
     });
 });
-
-module.exports = router;
 
 app.listen(process.env.PORT || 5000);
--- prove10/public/client.js
+++ prove10/public/client.js
@@ -1,16 +1,38 @@
 const fillList = () => {
     const nameList = document.getElementById('nameList')
 
-    fetch('/app/fetchAll')
+    fetch('/fetchAll')
         .then(res => res.json())
         .then(data => {
-            //Data code
+            nameList.innerHTML = '';
+
+            data.avengers.forEach(element => {
+                nameList.innerHTML += `<li>${element.name}</li>`;
+            });
         })
         .catch(err => {
             console.error(err)
         })
 }
 
-console.log(window.location);
+const addToList = () => {
+    const newHero = document.getElementById('newName').value
+
+    fetch('/insert', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ newHero })
+    })
+    .catch(err => {
+        document.getElementById('newName').value = ''
+        console.error(err)
+    })
+
+    document.getElementById('newName').value = '';
+
+    fillList()
+}
 
 fillList();
